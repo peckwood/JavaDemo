bd03d4eddd6d16cd74e6cc28d7847e7133e67f5a
raiden <fallenwood@qq.com>
2019-09-28 14:06:40 +0800

    Added p6, adding conditions to solve the problem of letting threads waiting in vain

---
+++ src/main/java/com/bladespear/demo/multithreading/p6_wait_notify_demo_multi_prod_consu_improved_condition/MainClassP6.java
@@ -0,0 +1,37 @@
+package com.bladespear.demo.multithreading.p6_wait_notify_demo_multi_prod_consu_improved_condition;
+
+import java.util.LinkedList;
+import java.util.concurrent.*;
+
+public class MainClassP6 {
+    public static void main(String[] args) throws InterruptedException {
+        LinkedList<Integer> queue = new LinkedList<>();
+        IntegerQueue integerQueue = new IntegerQueue(queue);
+        int maxCapacity = 4;
+        Producer5 producer = new Producer5(integerQueue, maxCapacity);
+        Consumer5 consumer = new Consumer5(integerQueue);
+
+        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(80);
+        CustomThreadFactory customThreadFactory = new CustomThreadFactory();
+        ExecutorService pool = new ThreadPoolExecutor(10, 20, 100, TimeUnit.SECONDS, workQueue, customThreadFactory);
+
+        pool.execute(producer);
+        pool.execute(producer);
+        pool.execute(producer);
+        pool.execute(producer);
+        pool.execute(producer);
+
+        pool.execute(consumer);
+        pool.execute(consumer);
+        pool.execute(consumer);
+        pool.execute(consumer);
+        pool.execute(consumer);
+
+        //guess how many what will 'workQueue.size()' print?
+//        for(int i =0 ;i<50;i++){
+//            pool.execute(producer);
+//            pool.execute(consumer);
+//        }
+//        System.out.println(workQueue.size());
+    }
+}
