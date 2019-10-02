085e6fe9db9a38d6f2294930ecfce780f8af7914
raiden <fallenwood@qq.com>
2019-10-02 22:27:39 +0800

    added files to git

---
+++ src/main/java/com/bladespear/demo/io/D32_Person.java
@@ -0,0 +1,37 @@
+package com.bladespear.demo.io;
+
+import java.io.Serializable;
+
+public class D32_Person implements Serializable {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "D32_Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public D32_Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
