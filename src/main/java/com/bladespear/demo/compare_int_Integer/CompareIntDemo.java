37a50591c7a8b76c64d9d80bc23b9dce306a233f
raiden <fallenwood@qq.com>
2019-10-10 10:58:55 +0800

    added comments

--- src/main/java/com/bladespear/demo/compare_int_Integer/CompareIntDemo.java
+++ src/main/java/com/bladespear/demo/compare_int_Integer/CompareIntDemo.java
@@ -6,9 +6,9 @@ public class CompareIntDemo {
         Integer b = Integer.valueOf(1);
         Integer c = new Integer(1);
 
-
+        //you can use == to compare Integer and int
         System.out.println("a == b: " + (a == b));
-
+        //you can use equals to compare Integer and int
         System.out.println("b.equals(a): " + b.equals(1));
         System.out.println("c.equals(a): " + c.equals(a));
 
