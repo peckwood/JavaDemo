d785caeb80ada7516992a68206f7ef66e5b723ec
raiden <fallenwood@qq.com>
2019-11-04 20:26:32 +0800

    json to csv conversion demo

---
+++ src/main/java/com/bladespear/demo/do_you_need_to_set/ParentObject.java
@@ -0,0 +1,25 @@
+package com.bladespear.demo.do_you_need_to_set;
+
+import java.util.List;
+
+public class ParentObject {
+    private List<Integer> list;
+
+
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public void setList(List<Integer> list) {
+        System.out.println("setter called");
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "ParentObject{" +
+                "list=" + list +
+                '}';
+    }
+}
