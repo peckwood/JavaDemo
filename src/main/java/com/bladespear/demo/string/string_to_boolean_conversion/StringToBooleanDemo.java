df3f9f44b189f22f0c52bc5c307ca5334f45a6fb
raiden <fallenwood@qq.com>
2019-11-04 21:00:43 +0800

    convert string to boolean

---
+++ src/main/java/com/bladespear/demo/string/string_to_boolean_conversion/StringToBooleanDemo.java
@@ -0,0 +1,11 @@
+package com.bladespear.demo.string.string_to_boolean_conversion;
+
+public class StringToBooleanDemo {
+    public static void main(String[] args) {
+        String trueString = "true";
+
+        boolean a = (boolean) trueString;
+        Boolean b = (Boolean) trueString;
+        boolean c = Boolean.valueOf(trueString)
+    }
+}
