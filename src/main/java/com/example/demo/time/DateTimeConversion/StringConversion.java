fc4db5aad5329125c4bbed8b544a4c0eb85254a7
fengxiao <fengxiao@sxylzinfo.com>
2025-05-21 17:22:47 +0800

    added 'how to format instant'

--- src/main/java/com/example/demo/time/DateTimeConversion/StringConversion.java
+++ src/main/java/com/example/demo/time/DateTimeConversion/StringConversion.java
@@ -1,16 +1,45 @@
 package com.example.demo.time.DateTimeConversion;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.junit.Test;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StringConversion {
+    private static Logger logger = LoggerFactory.getLogger(StringConversion.class);
+
     @Test
     public void timeToString(){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String string = now.format(formatter);
         System.out.println(string);
+    }
+
+    @Test
+    public void instantToString(){
+        Instant myInstant = Instant.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        try {
+            System.out.println(formatter.format(myInstant));
+        } catch (Exception e) {
+            logger.error("instant不能直接format, 会报错:", e);
+        }
+
+        //approach 1
+        // you need to add zone for formatter:
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault());
+        System.out.println(formatter2.format(myInstant));
+
+        //approach 1
+        // convert instant to ZonedDateTime
+        ZonedDateTime zonedDateTime = myInstant.atZone(ZoneId.systemDefault());
+        System.out.println(formatter.format(zonedDateTime));
+
     }
 }
